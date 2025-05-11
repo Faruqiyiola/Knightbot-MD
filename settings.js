3bdc71d481b383cbdcf1aeca4abcffa56bda45dd
Faruqiyiola <faruqiyiola692@gmail.com>
2025-05-11 10:46:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Lynx Bot', // Your name
+  botOwner: 'Rambo', // Your name
   ownerNumber: '2347015643335', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
